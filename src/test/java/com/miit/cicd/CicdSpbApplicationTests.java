6e624f73f44c627137ed5210b3daa674a001c1c8
chirag ahir <ahirchirag1459@gmail.com>
2023-03-04 14:11:16 -0500

    latest

---
+++ src/test/java/com/miit/cicd/CicdSpbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miit.cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdSpbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
